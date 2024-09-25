7951c094ece5372962de602ee70d430e595a51db
Nico <nicolas2289h@gmail.com>
2024-09-25 17:31:16 -0300

    refactor: se agregan metodos de spring security en UsuarioEntity; se cambia tipoUsuario a tipo Role

--- back/src/main/java/cl/chile/somosafac/entity/UsuarioEntity.java
+++ back/src/main/java/cl/chile/somosafac/entity/UsuarioEntity.java
@@ -1,15 +1,28 @@
 package cl.chile.somosafac.entity;
+
+import cl.chile.somosafac.security.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "usuarios")
-    public class UsuarioEntity {
+public class UsuarioEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,8 +34,9 @@ import java.time.LocalDateTime;
     @Column(name = "contrasena_hash", nullable = false)
     private String contrasenaHash;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "tipo_usuario", nullable = false)
-    private String tipoUsuario;
+    private Role tipoUsuario;
 
     @Column(name = "fecha_registro", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime fechaRegistro;
@@ -39,5 +53,40 @@ import java.time.LocalDateTime;
     @Column(name = "aceptar_terminos", nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
     private Boolean aceptarTerminos;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority((tipoUsuario.name())));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasenaHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return correo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
