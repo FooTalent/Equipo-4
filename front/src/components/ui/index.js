044164dbd7983f49f5c1a80d01f34383015d5ed7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-30 23:53:13 +0300

    feat: shadcn ui components

---
+++ front/src/components/ui/index.js
@@ -0,0 +1,7 @@
+export * from './button'
+export * from './alert-dialog'
+export * from './dialog'
+export * from './form'
+export * from './radio-group'
+export * from './input'
+export * from './label'
