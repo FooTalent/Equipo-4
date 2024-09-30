3b4da8cfe9cb5fc4a14a645fec13f679514087a6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-01 02:25:21 +0300

    feat: added routes

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Router from './routes/index.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App className="text-orange-700"/>
+    <Router />
   </StrictMode>,
-)
+);
