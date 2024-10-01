a775786a9fc657b13e1ba45bff10d3d523bf391f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-01 15:00:44 +0300

    feat: logo

--- front/src/modules/auth/page.jsx
+++ front/src/modules/auth/page.jsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 export default function Auth() {
   return (
     <main className='py-8 h-screen grid items-center px-4 text-center'>
-      <h1 className='self-end md:text-2xl lg:text-4xl'>
-        Bienvenido al Sistema de Gestión de Datos de <span className='font-bold'>AFAC</span>
-      </h1>
+      <div className=' flex flex-col gap-6 self-end text-left'>
+        <img src='/common/logo.svg' className='w-44'/>
+        <h1 className='text-4xl font-extralight'>
+        Bienvenido al <br /> Sistema de Gestión <br /> de Datos de <span className='font-bold'>AFAC</span>
+        </h1>
+      </div>
       <Link to={'/auth/tipo-usuario'} className="self-end bg-black text-base hover:opacity-80 py-3 text-white rounded-md">Continuar</Link>
     </main>
   );
