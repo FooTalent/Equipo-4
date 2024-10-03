d3b6c47dbac083b78306fc40f4fb550f8854b185
Nico <nicolas2289h@gmail.com>
2024-10-02 22:27:04 -0300

    refactor: se modifica la respuesta JSON de login para devolver datos del usuario

--- back/src/main/java/cl/chile/somosafac/controller/AuthController.java
+++ back/src/main/java/cl/chile/somosafac/controller/AuthController.java
@@ -1,11 +1,11 @@
 package cl.chile.somosafac.controller;
 
 import cl.chile.somosafac.DTO.UsuarioDTO;
-import cl.chile.somosafac.security.AuthResponse;
 import cl.chile.somosafac.service.AuthService;
 import cl.chile.somosafac.security.LoginRequest;
 import cl.chile.somosafac.security.RegisterRequest;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,22 +21,34 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<UsuarioDTO> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(authService.login(request, response));
     }
 
     @PostMapping(value = "register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
-        AuthResponse token = authService.register(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(token);
+    public ResponseEntity<UsuarioDTO> register(@RequestBody RegisterRequest request) {
+        UsuarioDTO usuarioRegistrado = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRegistrado);
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletRequest request) {
+    public ResponseEntity<String> logout(HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
             SecurityContextHolder.getContext().setAuthentication(null);
         }
+
+        // Invalida la cookie al cerrar sesion
+        Cookie jwtCookie = new Cookie("token", null);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setPath("/");
+        jwtCookie.setSecure(true);
+        jwtCookie.setMaxAge(0);
+        response.addCookie(jwtCookie);
+
+        System.out.println("Cookie login: " + jwtCookie.getName());
+        System.out.println(jwtCookie.getValue());
+
         return ResponseEntity.ok("Logout exitoso");
     }
 }
