c3eb15ead869150fa128095fbda16f203fe685e5
Nico <nicolas2289h@gmail.com>
2024-10-02 22:27:53 -0300

    refactor: se agrega cookie para el manejo de jwt

--- back/src/main/java/cl/chile/somosafac/service/AuthService.java
+++ back/src/main/java/cl/chile/somosafac/service/AuthService.java
@@ -1,14 +1,17 @@
 package cl.chile.somosafac.service;
 
+import cl.chile.somosafac.DTO.UsuarioDTO;
 import cl.chile.somosafac.entity.UsuarioEntity;
 import cl.chile.somosafac.repository.UsuarioRepository;
-import cl.chile.somosafac.security.AuthResponse;
 import cl.chile.somosafac.security.JwtService;
 import cl.chile.somosafac.security.LoginRequest;
 import cl.chile.somosafac.security.RegisterRequest;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,19 +26,29 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-    public AuthResponse login(LoginRequest request) {
+    public UsuarioDTO login(LoginRequest request, HttpServletResponse response) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getCorreo(), request.getContrasenaHash()));
-        UsuarioEntity usuario = usuarioRepository.findByCorreo(request.getCorreo()).orElseThrow();
+        UsuarioEntity usuario = usuarioRepository.findByCorreo(request.getCorreo()).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado."));
+
         usuario.setFechaUltimoAcceso(LocalDateTime.now());
         usuarioRepository.save(usuario);
 
         String token = jwtService.getToken(usuario);
-        return AuthResponse.builder()
-                .token(token)
-                .build();
+        // Configuracion de la cookie
+        Cookie jwtCookie = new Cookie("token", token);
+        jwtCookie.setHttpOnly(true);  // Evita que la cookie accesible desde JavaScript
+        jwtCookie.setPath("/");
+        jwtCookie.setSecure(true);
+        jwtCookie.setMaxAge(24 * 60 * 60); // Duracion de 1 dia - Consultar con equipo
+        response.addCookie(jwtCookie);
+
+        UsuarioDTO usuarioDTO = UsuarioDTO.fromEntity(usuario);
+        System.out.println("Cookie logout: " + jwtCookie.getName());
+        System.out.println(jwtCookie.getValue());
+        return usuarioDTO;
     }
 
-    public AuthResponse register(RegisterRequest request) {
+    public UsuarioDTO register(RegisterRequest request) {
         if (usuarioRepository.findByCorreo(request.getCorreo()).isPresent()) {
             throw new RuntimeException("El correo ya está en uso");
         }
@@ -51,8 +64,7 @@ public class AuthService {
 
         usuarioRepository.save(usuario);
 
-        return AuthResponse.builder()
-                .token(jwtService.getToken(usuario))
-                .build();
+        UsuarioDTO usuarioDTO = UsuarioDTO.fromEntity(usuario);
+        return usuarioDTO;
     }
 }
