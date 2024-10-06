99681af01c426684b9f63ccbe8e5ecd07613f719
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-07 02:16:06 +0300

    feat: added react query and toastify container

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Router from './routes/index.jsx';
+import ReactQueryProvider from './common/react-query-provider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <ReactQueryProvider>
+      <Router />
+      <ToastContainer />
+    </ReactQueryProvider>
   </StrictMode>,
 );
