777185ed00d73cbb62274e687c4e91ce394d5ad1
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-07 02:53:54 +0300

    fix: vite config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // eslint-disable-next-line no-undef
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    define: {
+      'import.meta.env.VITE_APP_API_URL': JSON.stringify(env.VITE_APP_API_URL),
+    },
+  };
 });
