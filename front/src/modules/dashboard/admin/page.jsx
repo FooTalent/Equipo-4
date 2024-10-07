0cbaf30445c37c93e6ac4d445ee5bb57fea3f480
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-07 04:01:18 +0300

    feat: admin dashboard

---
+++ front/src/modules/dashboard/admin/page.jsx
@@ -0,0 +1,11 @@
+import { AppLayout } from '@/layouts/app-layout';
+
+export default function AdminDashboard() {
+  return (
+    <AppLayout>
+      <section className='h-full p-0'>
+        <h2>Dashboard</h2>
+      </section>
+    </AppLayout>
+  );
+}
