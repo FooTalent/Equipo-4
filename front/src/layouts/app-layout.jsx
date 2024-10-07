7780b41db02f55b4a196b291e9b171c0bf0faae1
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-08 00:49:51 +0300

    feat: add protection against unauthenticated users

--- front/src/layouts/app-layout.jsx
+++ front/src/layouts/app-layout.jsx
@@ -1,8 +1,15 @@
 import Navbar from '@/components/shared/navbar';
 import Footer from '@/components/shared/footer';
+import useAuthStore from '@/store/user';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const AppLayout = ({ children }) => {
-
+  const navigate = useNavigate();
+  const user = useAuthStore((state) => state.user);
+  useEffect(() => {
+    if (!user) navigate('/auth');
+  }, [user, navigate]);
   return (
     <>
       <section className='w-full flex flex-col gap-y-4 md:gap-y-0 h-screen'>
