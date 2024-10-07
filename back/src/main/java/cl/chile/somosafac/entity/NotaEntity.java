661bbcb6d3733c1a6ee4372378eaca74513c39c8
Israel-Mege <isralmege@gmail.com>
2024-10-07 19:22:06 -0300

    feat(validations): Agrega validaciones a notasEntity y NotaDTO ademas se agreo jsonformat

--- back/src/main/java/cl/chile/somosafac/entity/NotaEntity.java
+++ back/src/main/java/cl/chile/somosafac/entity/NotaEntity.java
@@ -1,7 +1,9 @@
 package cl.chile.somosafac.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -15,18 +17,23 @@ public class NotaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "La familia no puede ser nula")
     @ManyToOne
     @JoinColumn(name = "familia_id", nullable = false)
     private FamiliaEntity familia;
 
+    @NotNull(message = "El voluntario no puede ser nulo")
     @ManyToOne
     @JoinColumn(name = "voluntario_id", nullable = false)
     private VoluntarioEntity voluntario;
 
+    @NotBlank(message = "La descripción no puede estar vacía")
+    @Size(min = 10, max = 500, message = "La descripción debe tener entre 10 y 500 caracteres")
     @Column(nullable = false)
     private String descripcion;
 
+    @NotNull(message = "La fecha de creación no puede ser nula")
+    @PastOrPresent(message = "La fecha de creación no puede ser futura")
     @Column(name = "fecha_creacion", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime fechaCreacion;
 }
-
