f777a697d1fb65a6f957f43a115ede3d25f5ab72
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-08 19:16:56 +0300

    style: fix spacing

--- front/src/modules/dashboard/admin/pages/profile.jsx
+++ front/src/modules/dashboard/admin/pages/profile.jsx
@@ -15,7 +15,7 @@ export default function AdminProfile() {
   return (
     <AppLayout>
       <section className='h-full md:bg-gray-100 md:grid md:items-center'>
-        <div className='p-0 grid md:flex md:flex-col gap-16 md:gap-12 max-w-6xl mx-auto px-2 md:px-4 md:py-4 md:h-3/4 md:bg-white md:w-[650px] md:rounded-lg md:border-0 md:mx-auto'>
+        <div className='p-0 grid md:flex md:flex-col gap-6 max-w-6xl mx-auto px-2 md:px-4 md:py-4 md:h-3/4 md:bg-white md:w-[650px] md:rounded-lg md:border-0 md:mx-auto'>
           <div className='flex md:flex-col gap-2 md:gap-5'>
             <img onClick={() => navigate('/admin/dashboard')} src='/common/arrow-left.svg' alt='Regresar a la página principal' className='self-start pt-2 md:pt-0 hover:cursor-pointer' />
             <p className='self-center -mb-2 md:-mb-0 text-2xl md:w-full'>Perfil Administrador</p>
--- front/src/modules/dashboard/admin/pages/request-mentoring.jsx
+++ front/src/modules/dashboard/admin/pages/request-mentoring.jsx
@@ -7,43 +7,46 @@ export default function RequestMentoring() {
   return (
     <AppLayout>
       <section className='h-full md:bg-gray-100 md:grid md:items-center'>
-        <div className='p-0 grid md:flex md:flex-col gap-16 md:gap-12 max-w-6xl mx-auto px-2 md:px-4 md:py-4 md:h-3/4 md:bg-white md:w-[650px] md:rounded-lg md:border-0 md:mx-auto'>
-          <div className='flex md:flex-col gap-2 md:gap-5'>
-            <img onClick={() => navigate('/admin/dashboard')} src='/common/arrow-left.svg' alt='Regresar a la página principal' className='self-start pt-2 md:pt-0 hover:cursor-pointer' />
-            <p className='self-center -mb-2 md:-mb-0 text-2xl md:w-full'>Solicitud de Mentorias</p>
+        <div className='p-0 grid md:flex md:flex-col gap-6 max-w-6xl mx-auto px-2 md:px-4 md:py-4 md:h-3/4 md:bg-white md:w-[650px] md:rounded-lg md:border-0 md:mx-auto'>
+          <div className='flex flex-col gap-4'>
+            <div className='flex md:flex-col gap-2 md:gap-5'>
+              <img onClick={() => navigate('/admin/dashboard')} src='/common/arrow-left.svg' alt='Regresar a la página principal' className='self-start pt-2 md:pt-0 hover:cursor-pointer' />
+              <p className='self-center -mb-2 md:-mb-0 text-2xl md:w-full'>Solicitud de Mentorias</p>
+            </div>
+            <p className='px-2 md:px-0 text-lf'>Estas son las familias que esperan por recibir mentorías:</p>
           </div>
           <div className='flex flex-col gap-3'>
             <div>
               <Input
-                className='border-orange-300 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
+                className='border-orange-500 border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
                 placeholder='Nombre y Apellido'
                 disabled
               />
             </div>
             <div>
               <Input
-                className='border-orange-300 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
+                className='border-orange-500 border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
                 placeholder='Nombre y Apellido'
                 disabled
               />
             </div>
             <div>
               <Input
-                className='border-orange-300 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
+                className='border-orange-500 border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
                 placeholder='Nombre y Apellido'
                 disabled
               />
             </div>
             <div>
               <Input
-                className='border-orange-300 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
+                className='border-orange-500 border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
                 placeholder='Nombre y Apellido'
                 disabled
               />
             </div>
             <div>
               <Input
-                className='border-orange-300 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
+                className='border-orange-500 border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-400'
                 placeholder='Nombre y Apellido'
                 disabled
               />
