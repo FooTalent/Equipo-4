de973018663bfb79b0c81a1106394c3ba510a3b0
nicolas2289h <nicolas2289h@gmail.com>
2024-10-09 14:55:56 -0300

    refactor: se modifica formato de los emails

--- back/src/main/java/cl/chile/somosafac/controller/EmailController.java
+++ back/src/main/java/cl/chile/somosafac/controller/EmailController.java
@@ -1,11 +1,17 @@
 package cl.chile.somosafac.controller;
 
+import cl.chile.somosafac.DTO.PasswordDTO;
 import cl.chile.somosafac.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@Tag(name = "Email", description = "Operaciones relacionadas con el envío de correos electrónicos")
 public class EmailController {
 
     private final EmailService emailService;
@@ -14,9 +20,21 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @PostMapping("/send")
-    public ResponseEntity<String> sendEmail(@RequestParam String email) {
-        emailService.enviarEmail(email, "Somos AFAC ✉\uFE0F", "Mensaje de prueba! \uD83D\uDE0E");
+
+    @Operation(summary = "Enviar email de registro", description = "Envía un email de bienvenida con las credenciales de acceso tras el registro del usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email enviado exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Error en la solicitud de envío de email")
+    })
+    @PostMapping("/mensaje-registro")
+    public ResponseEntity<String> sendEmailPresentacion(@RequestParam String email, @RequestBody PasswordDTO password) {
+        emailService.enviarEmailConPresentacion(
+                email,
+                "¡Bienvenido a la Asociación de Familias de Acogida de Chile (AFAC)! ✉️",
+                "",
+                password.getContrasenaHash());
+
         return ResponseEntity.ok("Email enviado exitosamente!");
     }
+
 }
--- back/src/main/java/cl/chile/somosafac/service/EmailService.java
+++ back/src/main/java/cl/chile/somosafac/service/EmailService.java
@@ -1,8 +1,14 @@
 package cl.chile.somosafac.service;
 
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @Service
 public class EmailService {
@@ -25,4 +31,34 @@ public class EmailService {
             throw new RuntimeException("Error al enviar email: " + e.getMessage());
         }
     }
+
+    public void enviarEmailConPresentacion(String email, String subject, String body, String password) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom("somosafac.dev@gmail.com");
+            helper.setTo(email);
+            helper.setSubject(subject);
+
+            var inputStream = Objects.requireNonNull(
+                    EmailService.class.getResourceAsStream("/template/email/emailBienvenida.html")
+            );
+            String htmlContent = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+            htmlContent = htmlContent.replace("{{email}}", email)
+                    .replace("{{passwordHash}}", password);
+
+            helper.setText(htmlContent, true);
+
+            ClassPathResource logo = new ClassPathResource("/static/images/portada.png");
+            helper.addInline("portada.png", logo);
+
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al enviar email: " + e.getMessage());
+        }
+    }
+
 }
