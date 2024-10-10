9560b6d1b15f81f5a05670e169cc0944696715fb
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-10 16:38:04 +0300

    fix: name's length

--- front/src/modules/dashboard/admin/schemas/schemaAdminProfile.js
+++ front/src/modules/dashboard/admin/schemas/schemaAdminProfile.js
@@ -4,8 +4,8 @@ export const schemaAdminProfile = yup.object({
   nombre: yup
     .string()
     .required('Introduce un nombre valido')
-    .min(2, 'Un nombre no puede tener menos de dos letras')
-    .max(20, 'Un nombre no puede tener más que 20 letras')
+    .min(3, 'Un nombre no puede tener menos de 3 letras')
+    .max(50, 'Un nombre no puede tener más que 50 letras')
     .matches(
       /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/,
       'Un nombre solo puede tener letras'
