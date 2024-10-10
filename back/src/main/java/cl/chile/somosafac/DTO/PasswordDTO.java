8b56cf0df4b75d084642bcc7524eede906d9dc99
nicolas2289h <nicolas2289h@gmail.com>
2024-10-09 21:41:06 -0300

    refactor: se agregan validaciones a PasswordDTO

--- back/src/main/java/cl/chile/somosafac/DTO/PasswordDTO.java
+++ back/src/main/java/cl/chile/somosafac/DTO/PasswordDTO.java
@@ -1,8 +1,19 @@
 package cl.chile.somosafac.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PasswordDTO {
+    @NotNull(message = "La contraseña no puede ser nula")
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+            message = "La contraseña debe tener al menos una letra, un número y un símbolo especial"
+    )
     private String contrasenaHash;
 }
