b9f9ade22f569393fb1d510adccc1e66c39066d6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-10 23:59:06 +0300

    feat: create user & send user data

--- front/src/modules/dashboard/admin/api/index.js
+++ front/src/modules/dashboard/admin/api/index.js
@@ -12,3 +12,30 @@ export const adminProfileApi = async (values) => {
       : 'Un error ha ocurrido';
   }
 };
+
+export const createUserApi = async (values) => {
+  try {
+    const response = await AxiosBase.post('/auth/register', values);
+    return response.data;
+  } catch (error) {
+    return axios.isAxiosError(error) &&
+      error.status >= 400 &&
+      error.status < 500
+      ? 'No se pudo crear el usuario'
+      : 'Un error ha ocurrido';
+  }
+};
+
+export const sendUserDataApi = async (values) => {
+  const { correo, contrasenaHash } = values;
+  try {
+    const response = await AxiosBase.post(`/email/mensaje-registro?email=${correo}`, { contrasenaHash });
+    return response.data;
+  } catch (error) {
+    return axios.isAxiosError(error) &&
+      error.status >= 400 &&
+      error.status < 500
+      ? 'No se pudo crear el usuario'
+      : 'Un error ha ocurrido';
+  }
+};
