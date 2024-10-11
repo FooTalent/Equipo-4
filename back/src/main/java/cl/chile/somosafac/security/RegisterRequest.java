80a75bffe723b5d725b31d9eab6a47724e1e0761
nicolas2289h <nicolas2289h@gmail.com>
2024-10-11 18:22:26 -0300

    feat: se agregan validaciones a Login y Registro

--- back/src/main/java/cl/chile/somosafac/security/RegisterRequest.java
+++ back/src/main/java/cl/chile/somosafac/security/RegisterRequest.java
@@ -1,5 +1,6 @@
 package cl.chile.somosafac.security;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +13,40 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotNull(message = "El nombre no puede ser nulo.")
+    @NotBlank(message = "El nombre no puede estar vacío.")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     String nombre;
+
+    @NotNull(message = "El apellido no puede ser nulo.")
+    @NotBlank(message = "El apellido no puede estar vacío.")
+    @Size(min = 3, max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
     String apellido;
+
+    @Email(message = "Debe ser un correo válido.")
     String correo;
+
+    @NotNull(message = "La contraseña no puede ser nula")
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+            message = "La contraseña debe tener al menos una letra, un número y un símbolo especial"
+    )
     String contrasenaHash;
+
+    @NotNull(message = "El tipo de Usuario debe ser ADMIN, FAMILIA o USUARIO")
     Role tipoUsuario;
+
     boolean activo = true;
+
     boolean verificado = false;
+
     LocalDateTime fechaUltimoAcceso;
+
     boolean aceptarTerminos;
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = Role.valueOf(tipoUsuario.toUpperCase());
+    }
 }
