f638f1d230d893e619f8ee529d2ea17d7ed09b5e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-17 01:58:44 +0300

    style: updated app's main colors

--- front/src/modules/auth/login/page.jsx
+++ front/src/modules/auth/login/page.jsx
@@ -92,7 +92,7 @@ const Login = () => {
   };
   return (
     <div className='relative md:bg-grayDefault py-8 h-screen grid md:flex md:flex-col md:gap-12 items-center px-4'>
-      <Link className='block md:hidden absolute z-30 top-8 left-4' to={'/auth/tipo-usuario'}><MdArrowBackIosNew /></Link>
+      <Link className='block md:hidden absolute z-30 top-8 left-4' to={'/auth'}><MdArrowBackIosNew /></Link>
       <Link to={'/auth'} className='hidden md:block top-4 left-4 w-full'>
         <img src='/common/logo-phrase.svg' alt='family one' className=''/>
       </Link>
