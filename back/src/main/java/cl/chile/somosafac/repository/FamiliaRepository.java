f2fee385fe153c0b5f3a68df423111b145d0be2a
nicolas2289h <nicolas2289h@gmail.com>
2024-10-18 14:58:50 -0300

    fix: se arregla el buscador de familias

--- back/src/main/java/cl/chile/somosafac/repository/FamiliaRepository.java
+++ back/src/main/java/cl/chile/somosafac/repository/FamiliaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FamiliaRepository extends JpaRepository<FamiliaEntity, Long> {
+    List<FamiliaEntity> findByNombreFaUnoContainingIgnoreCase(String nombre);
 
     @Query("SELECT f FROM FamiliaEntity f WHERE " +
             "(:nombre IS NULL OR f.nombreFaUno LIKE %:nombre%) AND " +
