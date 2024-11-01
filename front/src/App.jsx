9ae877815c487c81d30010f90ce980def7b0618d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-01 19:46:30 +0300

    feat: redirect user to auth

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,13 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './App.css';
 import './index.css';
 
 function App() {
-
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/auth');
+  }, [navigate]);
   return (
     <Link to={'/auth'}>Inicia sesión</Link>
   );
